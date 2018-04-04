bd57540cd2f1bf9718ddf4119ac128eb66f63d85
Lalit-Yadav02 <lalit.slg007@gmail.com>
2018-04-04 11:18:13 +0530

    Exception Handling

--- library/chapter2-node_essential_patterns/callback-convention.js
+++ library/chapter2-node_essential_patterns/callback-convention.js
@@ -1,6 +1,36 @@
+const fs = require('fs'); 
+/** Node convention of callbacks
 fs.readFile('foo.txt', 'utf8', (err, data) => { 
     if(err) 
         handleError(err); 
     else 
         processData(data); 
 });
+*/
+
+
+function readJSON(filename, callback) {
+    fs.readFile(filename, 'utf8', (err, data) => { 
+        let parsed; 
+        if(err) //propagate the error and exit the current function 
+            return callback(err);
+        try { 
+            //parse the file contents 
+            parsed = JSON.parse(data); } 
+        catch(err) {
+            //catch parsing errors
+            return callback(err); 
+        } 
+        //no errors, propagate just the data 
+        callback(null, parsed);
+        });
+}
+
+readJSON('foo.json', (err, data) => {
+    if(err)
+        console.log(err);
+    else
+        console.log(data);
+});
+
+
