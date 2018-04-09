664338b180a3a0dff88c53ea7d561fd4a76044aa
Lalit-Yadav02 <lalit.slg007@gmail.com>
2018-04-09 21:35:59 +0530

    Added web crawler

---
+++ library/chapter3-async_pattern/web-spider.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const fs = requires('fs');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const utilities = require('./utilities');
+function spider(url, callback) {
+    const filename = utilities.urlToFileName(url);
+    fs.exists(filename, exists => {
+        if(!exists) {
+            console.log(`Downloading ${url}`);
+            require(url, (err, response, body) => {
+                if(err){
+                    callback(err);
+                }else{
+                    mkdirp(path.dirname(filename), err => {
+                        if(err) {
+                            callback(err);
+                        }else{
+                            fs.writeFile(filename, body, err => {
+                                if(err) {
+                                    callback(err);
+                                }else{
+                                    callback(null, filename, true);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }else{
+            callback(null, filename, false);
+        }
+    });
+}
