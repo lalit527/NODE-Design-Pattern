b97a1b256d580e89e26436c96d6f0395f2c9c051
Lalit-Yadav02 <lalit.slg007@gmail.com>
2018-04-20 20:49:47 +0530

    Added program to limit the concurrent download in web-spider

---
+++ library/chapter3-async_pattern/web-spider-concurrent-limit.js
@@ -0,0 +1,27 @@
+const TaskQueue = require('./utilities/taskqueue'); 
+const downloadQueue = new TaskQueue(2);
+
+function spiderLinks(currentUrl, body, nesting, callback) { 
+    if(nesting === 0) { 
+        return process.nextTick(callback); 
+    }
+    const links = utilities.getPageLinks(currentUrl, body); 
+    if(links.length === 0) { 
+        return process.nextTick(callback); 
+    }
+    let completed = 0, hasErrors = false; 
+    links.forEach(link => { 
+        downloadQueue.pushTask(done => { 
+            spider(link, nesting - 1, err => { 
+                if(err) { 
+                    hasErrors= true; 
+                    return callback(err); 
+                } 
+                if(++completed === links.length && !hasErrors) { 
+                    callback(); 
+                } 
+                done(); 
+            }); 
+        }); 
+    });
+}
