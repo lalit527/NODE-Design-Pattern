ee478b6186643e891e5f970f0e503df8f86f6a41
Lalit-Yadav02 <lalit.slg007@gmail.com>
2018-04-23 20:45:50 +0530

    Promises with chaining

--- library/promises/basic.js
+++ library/promises/basic.js
@@ -8,9 +8,28 @@ asyncOperation(arg, (err, result) => {
     // do stuff with the result
 });
 
+/**
+ * Same operation with promises
+ */
 asynOperation(arg)
     .then(result => {
         // do stuff with the result
     }, err => {
         // handle error
     });
+
+/**
+ * Chaning of Promises
+ */
+asynOperation(arg)
+    .then(result1 => {
+        // do stuff with the result
+        return asyncOperation(arg2);
+    })
+    .then(result2 => {
+        // do stuff with the result
+        return 'done';
+    })
+    .then(undefined, err => {
+        //any error in the chain is caught here 
+    });;
