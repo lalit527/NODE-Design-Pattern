bd05fc7ff7db2fcbce4036991d094406cbe6f017
Lalit-Yadav02 <lalit.slg007@gmail.com>
2018-04-26 23:43:31 +0530

    Added program for file zip using buffer

---
+++ library/chapter5-streams/gzipBuf.js
@@ -0,0 +1,13 @@
+// ziping file using buffer
+const fs = require('fs');
+const zlib = require('zlib');
+
+const file = process.argv[2];
+
+fs.readFile(file, (err, buffer) => {
+    zlib.gzip(buffer, (err, buffer) => {
+        fs.writeFile(file + '.gz', buffer, err => {
+            console.log('File Compressed');
+        });
+    });
+});
