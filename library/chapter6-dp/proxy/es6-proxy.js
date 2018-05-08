dce1d443884f8cec0fb979289db58c9c718e088c
Lalit-Yadav02 <lalit.slg007@gmail.com>
2018-05-08 22:20:09 +0530

    proxy for even and odd

--- library/chapter6-dp/proxy/es6-proxy.js
+++ library/chapter6-dp/proxy/es6-proxy.js
@@ -10,3 +10,12 @@ const uppercaseScientist = new Proxy(scientist, {
 })
 
 console.log(uppercaseScientist.name, uppercaseScientist.surname);
+
+const evenNumbers = new Proxy([], {
+  get: (target, index) => index * 2,
+  has: (target, number) => number % 2 === 0
+});
+
+console.log(2 in evenNumbers);
+console.log(5 in evenNumbers);
+console.log(evenNumbers[7]);
