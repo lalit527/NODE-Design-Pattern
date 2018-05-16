32fee0601c9676e8806b6e08cf2883ff3a09022c
Lalit-Yadav02 <lalit.slg007@gmail.com>
2018-05-16 21:32:51 +0530

    added files for server

---
+++ library/chapter6-dp/state/server.js
@@ -0,0 +1,7 @@
+const server = jot.createServer(5000); 
+server.on('connection', socket => {
+  socket.on('data', data => {
+    console.log('Client data', data);
+  }); 
+}); 
+server.listen(5000, () => console.log('Started'));
