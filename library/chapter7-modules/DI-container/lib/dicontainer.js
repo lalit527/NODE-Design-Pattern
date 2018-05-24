409cf413604608d684945445352b66a094269c93
Lalit-Yadav02 <lalit.slg007@gmail.com>
2018-05-24 18:17:40 +0530

    Added DI Container

---
+++ library/chapter7-modules/DI-container/lib/dicontainer.js
@@ -0,0 +1,32 @@
+const fnArgs = require('parse-fn-args');
+
+module.exports = function() {
+  const dependencies = {};
+  const factories = {};
+  const diContainer = {};
+
+  diContainer.factory = (name, factory) => {
+    factories[name] = factory;
+  };
+
+  diContainer.register = (name, instance) => {
+    dependencies[name] = instance;
+  };
+
+  diContainer.get = (name) => {
+    if(!dependencies[name]) {
+      const factory = factories[name];
+      dependencies[name] = factory && diContainer.inject(factory);
+      if(!dependencies[name]) {
+        throw new Error('Cannot find Module: ' + name);
+      }
+    }
+    return dependencies[name];
+  }
+
+  diContainer.inject = (factory) => {
+    const args = fnArgs(factory)
+      .map(dependency => diContainer.get(dependency));
+        return factory.apply(null, args);
+  };
+}
